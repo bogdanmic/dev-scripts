dbf4b5090faa8adef6255bae7dc54bcf1ad76edc
bogdanmic <mic.bogdan@gmail.com>
2018-05-01 01:25:58 +0300

    Refactor the code and fix some comments.

--- setup.sh
+++ setup.sh
@@ -4,40 +4,56 @@
 # I use Ubuntu mainly. I'm not saying is the best choice nut it's ok.
 #
 
-// TODO: Maybe when adding content to the .bashrc file, instead create a file in
-// the WORK_PATH and add the changes inthere, then include that file into .bashrc
+# TODO: Maybe when adding content to the .bashrc file, instead create a file in
+# the SETUP_PATH and add the changes inthere, then include that file into .bashrc
+
+continueYesNo() {
+  echo -en "\e[32m${1} \e[34m[Y/n]\e[39m "
+  read -n 1 -r
+  echo    # (optional) move to a new line
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+    # 0 = true
+    return 0
+  else
+    # 1 = false
+    return 1
+  fi
+}
 
 echo -en "\e[32mEnter the PATH where to do the setup\e[34m[$(pwd)]:\e[39m: "
-read -e WORK_PATH
+read -e SETUP_PATH
 # If empty use the current directory
-name=${WORK_PATH:=$(pwd)}
-
-if [[ -d $WORK_PATH ]]; then
-    case "$WORK_PATH" in
-      */)
-        # The path has a trailing slach
-      ;;
-      *)
-        # The path doesn't have a trailing slach
-        WORK_PATH=$WORK_PATH/
-      ;;
-    esac
-    echo "Start working in [$WORK_PATH] ..."
+name=${SETUP_PATH:=$(pwd)}
+if [[ -d $SETUP_PATH ]]; then
+    # case "$SETUP_PATH" in
+    #   */)
+    #     # The path has a trailing slash
+    #   ;;
+    #   *)
+    #     # The path doesn't have a trailing slash
+    #     SETUP_PATH=$SETUP_PATH/
+    #   ;;
+    # esac
+    echo "Start working in [$SETUP_PATH] ..."
 else
-    echo -e "\e[31m[$WORK_PATH] is not valid directory.\e[39m"
+    echo -e "\e[31m[$SETUP_PATH] is not valid directory.\e[39m"
     exit 1
 fi
 
-read -p $'\e[32mInstall: git, git-flow ?[Y/n]\e[39m ' -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+# Define paths that we will use (All these have the slash because we added above)
+SETUP_PATH_SECRETS=$(realpath ${SETUP_PATH}/secrets)
+SETUP_PATH_WORK=$(realpath ${SETUP_PATH}/work)
+SETUP_PATH_CONTAINERS=$(realpath ${SETUP_PATH_CONTAINERS}/containers)
+SETUP_PATH_TOOLS=$(realpath ${WORK_PATH}/tools)
+SETUP_PATH_PRIVATE=$(realpath ${WORK_PATH}/private)
+
+ask="Install: git, git-flow?"
+if continueYesNo "$ask"; then
     sudo apt install -y git git-flow
 
-    read -p $'\e[32mConfigure  GIT ?[Y/n]\e[39m ' -n 1 -r
-    echo    # (optional) move to a new line
-    if [[ $REPLY =~ ^[Yy]$ ]]
-    then
+    ask="Configure  GIT?"
+    if continueYesNo "$ask"; then
         echo -n "Enter your user.name and press [ENTER]: "
         read username
         echo "!!! This email will be used when configureing your GitHub SSH key !!!"
@@ -49,41 +65,36 @@ then
         git config -l
     fi
 
-    read -p $'\e[32mSetup GitHub SSG key ?[Y/n]\e[39m ' -n 1 -r
-    echo    # (optional) move to a new line
-    if [[ $REPLY =~ ^[Yy]$ ]]
-    then
-        mkdir -p ${WORK_PATH}secrets
+    ask="Setup GitHub SSG key?"
+    if continueYesNo "$ask"; then
+        mkdir -p $SETUP_PATH_SECRETS
         mkdir ~/.ssh/
-        secrets_abs_path=$(realpath ${WORK_PATH}secrets)
-        ssh-keygen -t rsa -b 4096 -C "$(git config --global user.email)" -f $secrets_abs_path/id_rsa_github
-        ln -sf $secrets_abs_path/id_rsa_github ~/.ssh/
+
+        ssh-keygen -t rsa -b 4096 -C "$(git config --global user.email)" -f $SETUP_PATH_SECRETS/id_rsa_github
+        ln -sf $SETUP_PATH_SECRETS/id_rsa_github ~/.ssh/
         eval "$(ssh-agent -s)"
         echo "IdentityFile ~/.ssh/id_rsa_github" >> ~/.ssh/config
         ssh-add ~/.ssh/id_rsa_github
         echo -n "Enter your GitHub username and press [ENTER]: "
         read githubusername
         curl -u "$githubusername" \
-          --data "{\"title\":\"`lsb_release -ds`-`date +%Y-%m-%d-%H:%M:%S`\",\"key\":\"`cat $secrets_abs_path/id_rsa_github.pub`\"}" \
+          --data "{\"title\":\"`lsb_release -ds`-`date +%Y-%m-%d-%H:%M:%S`\",\"key\":\"`cat $SETUP_PATH_SECRETS/id_rsa_github.pub`\"}" \
           https://api.github.com/user/keys
     fi
 
     echo '!!! This might take quite a while. All = max 150 !!!'
-    read -p $'\e[32mClone all your GitHub repos ?[Y/n]\e[39m ' -n 1 -r
-    echo    # (optional) move to a new line
-    if [[ $REPLY =~ ^[Yy]$ ]]
-    then
-        mkdir -p ${WORK_PATH}work
-        cd ${WORK_PATH}work
+    ask="Clone all your GitHub repos?"
+    if continueYesNo "$ask"; then
+        mkdir -p $SETUP_PATH_WORK
+        cd $SETUP_PATH_WORK
+
         echo -n "Enter your GitHub username and press [ENTER]: "
         read githubusername
         curl -u "$githubusername" "https://api.github.com/user/repos?page=1&per_page=150" | grep -e 'ssh_url*' | cut -d \" -f 4 | xargs -L1 git clone
     fi
 
-    read -p $'\e[32mConfigure your Terminal prompt for GIT?[Y/n]\e[39m ' -n 1 -r
-    echo    # (optional) move to a new line
-    if [[ $REPLY =~ ^[Yy]$ ]]
-    then
+    ask="Configure your Terminal prompt for GIT?"
+    if continueYesNo "$ask"; then
         echo 'export PS1='\''${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\[\033[01;31m\]$(declare -F __git_ps1 &>/dev/null && __git_ps1 " (%s)")\[\033[00m\]\[\033[01;36m\]:\$\[\033[00m\] '\' >> ~/.bashrc
         echo 'export GIT_PS1_SHOWDIRTYSTATE=true' >> ~/.bashrc
         echo 'export GIT_PS1_SHOWUNTRACKEDFILES=true' >> ~/.bashrc
@@ -93,18 +104,14 @@ then
     echo "SUCCESS!"
 fi
 
-read -p $'\e[32mInstall: filezilla, vlc, virtualbox, firefox, vim, net-tools ?[Y/n]\e[39m ' -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+ask="Install: filezilla, vlc, virtualbox, firefox, vim, net-tools?"
+if continueYesNo "$ask"; then
     sudo apt install -y filezilla vlc virtualbox firefox vim net-tools
     echo "SUCCESS!"
 fi
 
-read -p $'\e[32mInstall: chrome ?[Y/n]\e[39m ' -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+ask="Install: chrome?"
+if continueYesNo "$ask"; then
     sudo add-apt-repository -y "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main"
     sudo curl -L https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
     sudo apt update
@@ -112,10 +119,8 @@ then
     echo "SUCCESS!"
 fi
 
-read -p $'\e[32mInstall: skype ?[Y/n]\e[39m ' -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+ask="Install: skype?"
+if continueYesNo "$ask"; then
     wget https://repo.skype.com/latest/skypeforlinux-64.deb
     sudo dpkg -i skypeforlinux-64.deb
     sudo apt install -y -f
@@ -123,10 +128,8 @@ then
     echo "SUCCESS!"
 fi
 
-read -p $'\e[32mInstall: dbeaver (sql client) ?[Y/n]\e[39m ' -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+ask="Install: dbeaver (sql client)?"
+if continueYesNo "$ask"; then
     wget https://dbeaver.jkiss.org/files/dbeaver-ce_latest_amd64.deb
     sudo dpkg -i dbeaver-ce_latest_amd64.deb
     sudo apt install -y -f
@@ -134,20 +137,16 @@ then
     echo "SUCCESS!"
 fi
 
-read -p $'\e[32mInstall: numix-icon-theme-circle ?[Y/n]\e[39m ' -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+ask="Install: numix-icon-theme-circle?"
+if continueYesNo "$ask"; then
     sudo add-apt-repository -y ppa:numix/ppa
     sudo apt update
     sudo apt install -y numix-icon-theme-circle
     echo "SUCCESS!"
 fi
 
-read -p $'\e[32mInstall: atom ide ?[Y/n]\e[39m ' -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+ask="Install: atom ide?"
+if continueYesNo "$ask"; then
     sudo curl -L https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
     sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
     sudo apt update
@@ -155,10 +154,8 @@ then
     echo "SUCCESS!"
 fi
 
-read -p $'\e[32mInstall: docker, docker-compose ?[Y/n]\e[39m ' -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+ask="Install: docker, docker-compose?"
+if continueYesNo "$ask"; then
     # Install docker
     sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
     sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -177,48 +174,40 @@ then
 
     echo "d[name] starts a container of the [name] service type"
     echo "e[name] executes a command in the container of the [name] service type"
-    read -p $'\e[32mAdd aliases for docker consul(dconsul,econsul), docker postgresql(dpostgres), docker pgadmin4(dpgadmin)?[Y/n]\e[39m ' -n 1 -r
-    echo    # (optional) move to a new line
-    if [[ $REPLY =~ ^[Yy]$ ]]
-    then
+    ask="Add aliases for docker consul(dconsul,econsul), docker postgresql(dpostgres), docker pgadmin4(dpgadmin)?"
+    if continueYesNo "$ask"; then
         echo "alias dconsul='docker run --rm -it --net=host --name dev-consul consul'" >> ~/.bashrc
         # This allows us to execute command inside the dev-consul container
         echo "alias econsul='docker exec dev-consul consul'" >> ~/.bashrc
-        mkdir -p $WORK_PATH/containers
-        containers_abs_path=$(realpath ${WORK_PATH}/containers)
+        mkdir -p $SETUP_PATH_CONTAINERS
+
         echo -n "Enter your POSTGRES_USER and press [ENTER]: "
         read postgresuser
         echo -n "Enter your POSTGRES_PASSWORD and press [ENTER]: "
         read postgrespassword
-        echo "alias dpostgres='docker run --rm -it -p 5432:5432 --name=dev-postgres -e POSTGRES_USER=$postgresuser -e POSTGRES_PASSWORD=$postgrespassword -v $containers_abs_path/postgres_home:/var/lib/postgresql/data postgres -c \"log_statement=all\" -c \"log_duration=on\" -c \"log_min_duration_statement=-1\"'" >> ~/.bashrc
-        mkdir -p $containers_abs_path/pgadmin_home
-        sudo chmod -R 777 $containers_abs_path/pgadmin_home
-        echo "alias dpgadmin='docker run --rm -it --net=host --name=dev-pgadmin -v $containers_abs_path/pgadmin_home:/pgadmin thajeztah/pgadmin4'" >> ~/.bashrc
+        echo "alias dpostgres='docker run --rm -it -p 5432:5432 --name=dev-postgres -e POSTGRES_USER=$postgresuser -e POSTGRES_PASSWORD=$postgrespassword -v $SETUP_PATH_CONTAINERS/postgres_home:/var/lib/postgresql/data postgres -c \"log_statement=all\" -c \"log_duration=on\" -c \"log_min_duration_statement=-1\"'" >> ~/.bashrc
+        mkdir -p $SETUP_PATH_CONTAINERS/pgadmin_home
+        sudo chmod -R 777 $SETUP_PATH_CONTAINERS/pgadmin_home
+        echo "alias dpgadmin='docker run --rm -it --net=host --name=dev-pgadmin -v $SETUP_PATH_CONTAINERS/pgadmin_home:/pgadmin thajeztah/pgadmin4'" >> ~/.bashrc
     fi
-
     echo "SUCCESS!"
 fi
 
-read -p $'\e[32mInstall: java8 ?[Y/n]\e[39m ' -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+ask="Install: java8?"
+if continueYesNo "$ask"; then
     sudo add-apt-repository -y ppa:webupd8team/java
     sudo apt update
     sudo apt install -y oracle-java8-installer
 
-    read -p $'\e[32mFetch from web: maven, activator, JetBrains ToolBox ?[Y/n]\e[39m ' -n 1 -r
-    echo    # (optional) move to a new line
-    if [[ $REPLY =~ ^[Yy]$ ]]
-    then
+    ask="Install: maven, activator, JetBrains ToolBox?"
+    if continueYesNo "$ask"; then
         sudo apt install -y zip gzip tar
-        mkdir -p $WORK_PATH/tools
-        tools_abs_path=$(realpath ${WORK_PATH}tools)
+        mkdir -p $SETUP_PATH_TOOLS
 
         # Get maven
-        wget -qO- http://mirrors.m247.ro/apache/maven/maven-3/3.5.3/binaries/apache-maven-3.5.3-bin.tar.gz | tar xvz -C $tools_abs_path
+        wget -qO- http://mirrors.m247.ro/apache/maven/maven-3/3.5.3/binaries/apache-maven-3.5.3-bin.tar.gz | tar xvz -C $SETUP_PATH_TOOLS
         # Add maven to PATH
-        echo "PATH=\$PATH:$tools_abs_path/apache-maven-3.5.3/bin" >> ~/.bashrc
+        echo "PATH=\$PATH:$SETUP_PATH_TOOLS/apache-maven-3.5.3/bin" >> ~/.bashrc
         echo 'export MAVEN_OPTS="-Xmx512m"' >> ~/.bashrc
 
         # Get nodejs (This gets installed by yarn so we don't do this for now)
@@ -227,34 +216,29 @@ then
         # echo "PATH=\$PATH:$tools_abs_path/node-v8.11.1-linux-x64/bin" >> ~/.bashrc
 
         # Get typesafe activator
-        wget  http://downloads.typesafe.com/typesafe-activator/1.3.12/typesafe-activator-1.3.12-minimal.zip -P $tools_abs_path
-        unzip -o $tools_abs_path/typesafe-activator-1.3.12-minimal.zip -d $tools_abs_path && rm $tools_abs_path/typesafe-activator-1.3.12-minimal.zip
+        wget  http://downloads.typesafe.com/typesafe-activator/1.3.12/typesafe-activator-1.3.12-minimal.zip -P $SETUP_PATH_TOOLS
+        unzip -o $SETUP_PATH_TOOLS/typesafe-activator-1.3.12-minimal.zip -d $SETUP_PATH_TOOLS && rm $SETUP_PATH_TOOLS/typesafe-activator-1.3.12-minimal.zip
         # Add activator to PATH
-        echo "PATH=\$PATH:$tools_abs_path/activator-1.3.12-minimal/bin" >> ~/.bashrc
+        echo "PATH=\$PATH:$SETUP_PATH_TOOLS/activator-1.3.12-minimal/bin" >> ~/.bashrc
 
         # Get JetBrains ToolBox app that makes it easier to update InteliJ ad get it.
-        wget -qO- https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.8.3678.tar.gz | tar xvz -C $tools_abs_path
+        wget -qO- https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.8.3678.tar.gz | tar xvz -C $SETUP_PATH_TOOLS
         echo "SUCCESS!"
     fi
-
     echo "SUCCESS!"
 fi
 
-read -p $'\e[32mInstall: yarn and node ?[Y/n]\e[39m ' -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+ask="Install: yarn and node?"
+if continueYesNo "$ask"; then
     curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
     echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
     sudo apt update && sudo apt install -y yarn
     echo "SUCCESS!"
 fi
 
-read -p $'\e[32mAdd any private aliases found in '${WORK_PATH}$'private/aliases file?[Y/n]\e[39m ' -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    FILE=${WORK_PATH}private/aliases
+ask="Add any private aliases found in ${SETUP_PATH_PRIVATE}/aliases file?"
+if continueYesNo "$ask"; then
+    FILE=$SETUP_PATH_PRIVATE/aliases
     if [ -f $FILE ]; then
       // TODO: Instead of appending the file inside .bashrc, include it so when
       // you add more aliases they are taken into consideration
@@ -265,10 +249,8 @@ then
     fi
 fi
 
-read -p $'\e[32mInstall awscli?[Y/n]\e[39m ' -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+ask="Install awscli?"
+if continueYesNo "$ask"; then
   sudo apt install -y python-pip
   pip install awscli --upgrade --user
   echo "PATH=\$PATH:~/.local/bin" >> ~/.bashrc
@@ -284,9 +266,7 @@ echo "Cheching for updates ..."
 sudo apt update
 sudo apt -y upgrade
 
-read -p $'\e[31mReboot?[Y/n]\e[39m ' -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+ask="31mReboot?"
+if continueYesNo "$ask"; then
     sudo reboot
 fi
